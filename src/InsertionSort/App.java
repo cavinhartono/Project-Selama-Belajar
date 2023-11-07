0098fee74d21692d544a5c631d511ecde3337a60
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-11-07 20:01:21 +0700

    main

--- src/InsertionSort/App.java
+++ src/InsertionSort/App.java
@@ -11,14 +11,13 @@ class InsertionSort {
 
   public void sort(int nums[]) {
     for (int i = 0; i < nums.length; i++) {
-      int key = nums[i];
-      int j = i - 1;
+      int key = nums[i], j = i - 1;
 
       while (j >= 0 && nums[j] > key) {
         nums[j + 1] = nums[j];
         j -= 1;
       }
-
+      
       nums[j + 1] = key;
 
     }
