4913a3e642d8e77c8e96d828d02fbbecda1edcb7
Cavin Hartono Putra <77716310+cavinhartono@users.noreply.github.com>
2023-11-14 21:44:31 +0700

    Add files via upload

--- src/Programming/Basic/App.java
+++ src/Programming/Basic/App.java
@@ -4,33 +4,58 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    String name;
-    int age;
-    double height;
-    char gender;
-    boolean isMarried;
+    boolean isActive = true;
 
-    Scanner input = new Scanner(System.in);
-    
-    System.out.print("Masukan nama Anda           : ");
-    name = input.nextLine();
+    Scanner ip = new Scanner(System.in);
 
-    System.out.print("Masukan umur Anda           : ");
-    age = input.nextInt();
+    do {
+      clearScreen();
 
-    System.out.print("Masukan jenis kelamin Anda  : ");
-    gender = input.next().charAt(0);
+      System.out.println("1. List Buku");
+      System.out.println("2. Cari Buku");
+      System.out.println("3. Tambah Buku");
+      System.out.println("4. Ubah Buku");
+      System.out.println("5. Hapus Buku");
 
-    System.out.print("Masukan tinggi badan Anda   : ");
-    height = input.nextDouble();
+      System.out.print("\n Pilihan Anda: ");
+      int choice = ip.nextInt();
 
-    System.out.print("Masukan status menikah Anda : ");
-    isMarried = input.nextBoolean();
+      switch (choice) {
+        case 1:
+          System.out.println("List Buku \n");
+          break;
+        case 2:
+          System.out.println("Cari Buku \n");
+          
+          break;
+        case 3:
+          System.out.println("Tambah Buku \n");
+          
+          break;
+        case 4:
+          System.out.println("Ubah Buku \n");
+          
+          break;
+        case 5:
+          System.out.println("Hapus Buku \n");
+          
+          break;
+        default:
+          break;
+      }
 
-    System.out.println("Nama            : " + name);
-    System.out.println("Umur            : " + age);
-    System.out.println("Jenis Kelamin   : " + gender);
-    System.out.println("Tinggi Badan    : " + height);
-    System.out.println("Status Menikah  : " + isMarried);
+    } while (isActive);
+  }
+
+  private static void clearScreen() {
+    try {
+      if (System.getProperty("os.name").contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        System.out.println("\033\143");
+      }
+    } catch (Exception e) {
+      System.err.println("Tidak bisa melakukan clean screen");
+    }
   }
 }
