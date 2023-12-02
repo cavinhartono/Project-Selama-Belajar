e61e8301436f814fc14065284cf39d023896467c
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2023-12-02 17:58:57 +0700

    main

---
+++ src/Library/Main.java
@@ -0,0 +1,7 @@
+package Library;
+
+public class Main {
+  public static void main(String[] args) {
+    Library obj = new Library();
+  }
+}
--- src/PrimeNumbers/App.java
+++ src/PrimeNumbers/App.java
@@ -1,3 +1,5 @@
+package PrimeNumbers;
+
 public class App {
   public static void main(String[] args) {
     // String primeNumbers = "";
