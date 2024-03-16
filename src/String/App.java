946de65c0e0f0e862a2025b9607173aeeac53753
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-03-16 07:40:13 +0700

    main

--- src/String/App.java
+++ src/String/App.java
@@ -19,6 +19,7 @@ public class App {
     String aToSubString = a.substring(2, 7);
     String x = aToSubString + b;
     System.out.println(x);
+
     // 2. y = length(x);
     int y = x.length();
 
@@ -38,19 +39,22 @@ public class App {
 
     // 5. d = delete(concat(substr(a, 2, 3), c), 2, 2);
     // Tanpa menggunakan Library 
-    String afterSubstringAndConcatText = (a.substring(1, 4) + c);
-    String d = obj.delete(afterSubstringAndConcatText, 1, 3);
+    String dSubstring = a.substring(1, 4);
+    String dConcat = dSubstring + c;
+    String d = obj.delete(dConcat, 1, 3);
 
     // Menggunakan Library
-    StringBuilder dWithStringBuilder = new StringBuilder(afterSubstringAndConcatText).delete(1, 3);
+    StringBuilder dWithStringBuilder = new StringBuilder(a.substring(1, 4) + cWithStringBuilder)
+                                                        .delete(1, 3);
 
+    // Output
     System.out.println("1. '" + x + "'");
     System.out.println("2. " + y + " (dihitung tanpa tanda kutip)");
     System.out.println("3. '" + z + "' (tanpa library)");
-    System.out.println("   '" + zWithStringBuilder + "' (dengan library)");
+    System.out.println("   '" + zWithStringBuilder + "' (dengan StringBuilder)");
     System.out.println("4. '" + c + "'");
-    System.out.println("   '" + cWithStringBuilder + "' (dengan library)");
+    System.out.println("   '" + cWithStringBuilder + "' (dengan StringBuilder)");
     System.out.println("5. '" + d + "'");
-    System.out.println("   '" + dWithStringBuilder + "' (dengan library)");
+    System.out.println("   '" + dWithStringBuilder + "' (dengan StringBuilder)");
   }
 }
