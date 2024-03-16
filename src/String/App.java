3d1379461d873b6857d567e97cd4cd8cd0ae4b2d
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-03-16 08:00:03 +0700

    main

--- src/String/App.java
+++ src/String/App.java
@@ -16,18 +16,19 @@ public class App {
     String a = "Teknik informatika", b = "Sisteminformasi";
 
     // 1. x = concat(substr(a, 3, 5), b);
-    String aToSubString = a.substring(2, 7);
-    String x = aToSubString + b;
+    String aSubString = a.substring(2, 7);
+    
+    String x = aSubString + b;
     System.out.println(x);
 
     // 2. y = length(x);
     int y = x.length();
 
     // 3. z = insert(x, b, 3);
-    // - Tanpa menggunakan Library
+    // Tanpa menggunakan Library
     String z = obj.insert(x, b, 2);
 
-    // - Menggunakan Library
+    // Menggunakan Library
     StringBuilder zWithStringBuilder = new StringBuilder(x).insert(2, b);
 
     // 4. c = delete(a, 4, 5);
@@ -45,7 +46,7 @@ public class App {
 
     // Menggunakan Library
     StringBuilder dWithStringBuilder = new StringBuilder(a.substring(1, 4) + cWithStringBuilder)
-                                                        .delete(1, 3);
+                                                          .delete(1, 3);
 
     // Output
     System.out.println("1. '" + x + "'");
