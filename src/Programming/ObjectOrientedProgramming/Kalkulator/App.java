b9a9188743b7eb26b096fb5c84efe0e850b046fd
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-03-25 10:41:11 +0700

    main

--- src/Programming/ObjectOrientedProgramming/Kalkulator/App.java
+++ src/Programming/ObjectOrientedProgramming/Kalkulator/App.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class App {
   private static Scanner ip = new Scanner(System.in);
   public static void main(String[] args) {
-    double x = 0, y = 0, result = 0;
+    double x = 0, y = 0, calculated;
+    String result = null;
     char loop = 'n';
     int choice = 0;
 
@@ -15,40 +16,45 @@ public class App {
       System.out.println("2. Pengurangan");
       System.out.println("3. Perkalian");
       System.out.println("4. Pembagian");
-
       System.out.print("Pilihan: ");
       choice = ip.nextInt();
-
-      System.out.print("Masukan nilai X               : ");
-      x = ip.nextDouble();
       
-      System.out.print("Masukan nilai Y               : ");
-      y = ip.nextDouble();
+      if (choice < 5) {
+        System.out.print("Masukan nilai X               : ");
+        x = ip.nextDouble();
+
+        System.out.print("Masukan nilai Y               : ");
+        y = ip.nextDouble();
+      } else {
+        result = "Error: Pilihan anda tidak ada!";
+      }
 
       switch (choice) {
         case 1:
-          result = x + y;
+          calculated = x + y;
+          result = "Hasil: " + calculated;
           break;
         case 2:
-          result = x - y;  
+          calculated = x - y;
+          result = "Hasil: " + calculated;
           break;
         case 3:
-          result = x * y;
+          calculated = x * y;
+          result = "Hasil: " +  calculated;
           break;
         case 4:
-          if (y != 0) 
-            result = x / y;
-          else 
-            System.err.println("\nError: Tidak dapat membagi dengan nol!");
-          break;
-        default:
-          System.err.println("\nError: Pilihan Anda tidak ada!");
+          if (y != 0) {
+            calculated = x / y;
+            result = "Hasil: " +  calculated;
+          } else {
+            result = "Error: Tidak dapat membagi dengan nol!";
+          }
           break;
       }
 
-      System.out.println("\nHasil                         : " + result);
+      System.out.println("\n" + result);
 
-      System.out.print("Apakah ingin melakukan perhitungan lagi? (y/n) ");
+      System.out.print("\nApakah ingin melakukan perhitungan lagi? (y/n) ");
       loop = ip.next().charAt(0);
     } while (Character.toLowerCase(loop) == 'y');
   }
