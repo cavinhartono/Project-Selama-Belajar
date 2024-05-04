118f2880a51087409f745056dfc6e7607f4dd5cf
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-05-04 19:36:24 +0700

    main

---
+++ src/Programming/FindGrade/App.java
@@ -0,0 +1,33 @@
+package Programming.FindGrade;
+
+import java.util.Scanner;
+
+class FindGrade {
+  public String getIndexGrade(int score) {
+    String[] Grades = { "A", "B", "C", "D", "E" };
+    String result = "Invalid score.";
+
+    if(score >= 0 && score < 60) {
+      result = Grades[Grades.length - 1];
+    } else if(score >= 60 && score < 85) {
+      result = Grades[Grades.length - (score) - 60 / 5];
+    } else if(score >= 85) {
+      result = Grades[0];
+    }
+
+    return result;
+  }
+
+  public String findGrade(int score) {
+    String result = "Invaild score";
+
+    return result;
+  }
+}
+
+public class App {
+  private static Scanner ip = new Scanner(System.in);
+  public static void main(String[] args) {
+    
+  }
+}
