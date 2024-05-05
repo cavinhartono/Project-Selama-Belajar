d7ac76942efcb29de251fbd74dd02ac2fef1aba0
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-05-05 19:48:48 +0700

    main

--- src/FindGrade/App.java
+++ src/FindGrade/App.java
@@ -3,31 +3,56 @@ package FindGrade;
 import java.util.Scanner;
 
 class FindGrade {
-  public String getIndexGrade(int score) {
-    String[] Grades = { "A", "B", "C", "D", "E" };
-    String result = "Invalid score.";
-
-    if(score >= 0 && score < 60) {
-      result = Grades[Grades.length - 1];
-    } else if(score >= 60 && score < 85) {
-      result = Grades[Grades.length - (score) - 60 / 5];
-    } else if(score >= 85) {
-      result = Grades[0];
+  public double avarageGrade(int[] scores) {
+    double sum = 0;
+    for (int score : scores) {
+      sum += score;
     }
 
-    return result;
+    return sum / scores.length;
   }
 
-  public String findGrade(int score) {
-    String result = "Invaild score";
+  public char[] getGrade(int[] scores) {
+    char[] results = new char[scores.length];
+    
+    for (int i = 0; i < scores.length; i++) {
+      if(scores[i] >= 0 && scores[i] < 50) {
+        results[i] = 'E';
+      } else if(scores[i] >= 50 && scores[i] < 65) {
+        results[i] = 'D';
+      } else if(scores[i] >= 65 && scores[i] < 75) {
+        results[i] = 'C';
+      } else if(scores[i] >= 75 && scores[i] < 85) {
+        results[i] = 'B';
+      } else {
+        results[i] = 'A';
+      }
+    }
 
-    return result;
+    return results;
+  }
+
+  public void printGrades(String[] Students, char[] Grades) {
+    for (String name : Students) {
+      for (char grade : Grades) {
+        System.out.println(name + ": " + grade);
+      }
+    }
   }
 }
 
 public class App {
   private static Scanner ip = new Scanner(System.in);
   public static void main(String[] args) {
-    
+    FindGrade obj = new FindGrade();
+
+    String[] Students = new String[ip.nextInt()];
+    int[] Scores = new int[Students.length];
+    for(int i = 0; i <= Students.length; i++) {
+      Students[i] = ip.next();
+      Scores[i] = ip.nextInt();
+    }
+
+    obj.printGrades(Students, obj.getGrade(Scores));
   }
 }
