7f79fa298918d626682c17dc416c4467cab0987b
Cavin Hartono Putra <muhammadcavin25@gmail.com>
2024-05-07 22:15:25 +0700

    main

---
+++ src/Programming/ObjectOrientedProgramming/Method/GeometricAreaCalculator.java
@@ -0,0 +1,11 @@
+package Programming.ObjectOrientedProgramming.Method;
+
+public class GeometricAreaCalculator {
+  public double circle(double radius) {
+    return 3.14 * radius * radius;
+  }
+
+  public double square() {
+    return;
+  }
+}
